97afb2a19912e7ad4c3d1ff663b9aef9d7df0cff
delarum <delarum7@gmail.com>
2026-04-07 23:15:52 +0300

    add background content to herobackgroud.jsx

---
+++ src/components/HeroBackground.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function HeroBackground() {
+  return (
+    <div className="hero-bg">
+      <div className="wave wave-1"></div>
+      <div className="wave wave-2"></div>
+      <div className="wave wave-3"></div>
+
+      <div className="floating-circle circle-1"></div>
+      <div className="floating-circle circle-2"></div>
+      <div className="floating-circle circle-3"></div>
+      <div className="floating-circle circle-4"></div>
+      <div className="floating-circle circle-5"></div>
+      <div className="floating-circle circle-6"></div>
+    </div>
+  );
+}
+
+export default HeroBackground;
