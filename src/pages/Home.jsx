198cd026c5151d40b51f4679166057841a27720d
delarum <delarum7@gmail.com>
2026-04-11 16:56:34 +0300

    update my home section to accomodate the map and hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,36 @@ import Navbar from '../components/navbar';
 import '../styles/navbar.css';
 import Footer from '../components/Footer';
 import '../styles/footer.css';
+import MapSection from '../components/MapSection';
+import '../styles/home.css';
 
 function Home() {
   return (
     <>
       <Navbar />
+      <div className="home">
+
+      {/* HERO */}
+      <section className="hero">
+        <div className="hero-overlay">
+          <h1>Protecting Our Life</h1>
+          <p>
+            Intelligent water tracking across Nairobi River Basin
+          </p>
+          <button className="cta-btn">View Live Tracking</button>
+        </div>
+      </section>
+
+      {/* MAP */}
+      <section className="map-wrapper">
+        <h2>The Nairobi River Basin Monitoring Map</h2>
+        <MapSection />
+        <p className="map-description">
+          HYDROS-C monitors pollution and ecosystem health across Nairobi.
+        </p>
+      </section>
+
+    </div>
       <Footer />
     </>
   );
